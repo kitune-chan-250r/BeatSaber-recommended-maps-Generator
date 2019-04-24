90cb36e534188558b281ae4e35885f95c3dd22ba
fox100 <nekoo.hsiui@gmail.com>
2019-04-24 21:35:28 +0900

    2

--- kivytest.py
+++ kivytest.py
@@ -1,45 +1,31 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty 
+from kivy.uix.recycleview import RecycleView
+from kivy.lang import Builder
+from kivy.uix.image import Image
+"""
+class SongnameView(RecycleView):
+    def __init__(self, **kwargs):
+        super(SongnameView, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in range(5)]
+"""
+class SongImageView(RecycleView):
+    def __init__(self, **kwargs):
+        super(SongImageView, self).__init__(**kwargs)
+        self.data = [{"source": "kivy.png"} for x in range(5)]
 
 
-items = [
-    {"color":(1, 1, 1, 1), "font_size": "20sp", "text": "white",     "input_data": ["some","random","data"]},
-    {"color":(.5,1, 1, 1), "font_size": "30sp", "text": "lightblue", "input_data": [1,6,3]},
-    {"color":(.5,.5,1, 1), "font_size": "40sp", "text": "blue",      "input_data": [64,16,9]},
-    {"color":(.5,.5,.5,1), "font_size": "70sp", "text": "gray",      "input_data": [8766,13,6]},
-    {"color":(1,.5,.5, 1), "font_size": "60sp", "text": "orange",    "input_data": [9,4,6]},
-    {"color":(1, 1,.5, 1), "font_size": "50sp", "text": "yellow",    "input_data": [852,958,123]}
-]
-
-
-class MyButton(Button):
-
-    def print_data(self,data):
-        print(data)
-
-
-KV = '''
-
-<MyButton>:
-    on_release:
-        root.print_data(self.input_data)
-
-RecycleView:
-    data: []
-    viewclass: 'MyButton'
-    RecycleBoxLayout:
-        default_size_hint: 1, None
-        orientation: 'vertical'
-
-'''
 
 
 class Test(App):
     def build(self):
-        root = Builder.load_string(KV)
-        root.data = [item for item in items]
-        return root
+        #root = Builder.load_string(KV)
+        #root.data = [item for item in items]
+        return RecycleView()
 
 
 Test().run()
